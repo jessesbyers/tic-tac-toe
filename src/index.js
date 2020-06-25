1b58ad4015fe2cb576761d66051aeddba63b90ba
Jesse Byers <jesse.s.byers@gmail.com>
2020-06-25 06:54:46 -0400

    added react form

--- src/index.js
+++ src/index.js
@@ -165,4 +165,182 @@ ReactDOM.render(
     <Game />,
     document.getElementById('root')
 );
+
+
+
+
+
+
+// import React, {Component} from 'react';
+// import ReactDOM from 'react-dom';
+
+// const style = {
+//   table: {
+//     borderCollapse: 'collapse'
+//   },
+//   tableCell: {
+//     border: '1px solid gray',
+//     margin: 0,
+//     padding: '5px 10px',
+//     width: 'max-content',
+//     minWidth: '150px'
+//   },
+//   form: {
+//     container: {
+//       padding: '20px',
+//       border: '1px solid #F0F8FF',
+//       borderRadius: '15px',
+//       width: 'max-content',
+//       marginBottom: '40px'
+//     },
+//     inputs: {
+//       marginBottom: '5px'
+//     },
+//     submitBtn: {
+//       marginTop: '10px',
+//       padding: '10px 15px',
+//       border:'none',
+//       backgroundColor: 'lightseagreen',
+//       fontSize: '14px',
+//       borderRadius: '5px'
+//     }
+//   }
+// }
+
+// class PhoneBookForm extends Component {
+
+//   state = {
+//     userFirstName: '',
+//     userLastName: '', 
+//     userPhone: ''
+//   }
+
+//   handleChange = (e) => {
+//     e.preventDefault()
+//     this.setState({
+//       [e.target.name]: [e.target.value]
+//     })
+//   }
+
+//   handleSubmit = (e) => {
+//     e.preventDefault()
+//         // call function in Application component to add data to phoneBook
+//     this.props.addEntry(this.state)   
+//         this.setState({
+//             userFirstname: '',
+//             userLastname: '', 
+//             userPhone: ''
+//         })
+//   }
+
+//   render() {
+//     return (
+//       <form onSubmit={this.handleSubmit.bind(this)} style={style.form.container}>
+//         <label>First name:</label>
+//         <br />
+//         <input 
+//           style={style.form.inputs}
+//           className='userFirstname'
+//           name='userFirstname' 
+//           type='text'
+//           placeholder='Coder'
+//           value={this.state.userFirstname}
+//           onChange={this.handleChange.bind(this)}
+//         />
+//         <br/>
+//         <label>Last name:</label>
+//         <br />
+//         <input 
+//           style={style.form.inputs}
+//           className='userLastname'
+//           name='userLastname' 
+//           type='text' 
+//           placeholder='Byte'
+//           value={this.state.userLastname}
+//           onChange={this.handleChange.bind(this)}
+
+//         />
+//         <br />
+//         <label>Phone:</label>
+//         <br />
+//         <input
+//           style={style.form.inputs}
+//           className='userPhone' 
+//           name='userPhone' 
+//           type='text'
+//           placeholder='8885559999'
+//           value={this.state.userPhone}
+//           onChange={this.handleChange.bind(this)}
+//         />
+//         <br/>
+//         <input 
+//           style={style.form.submitBtn} 
+//           className='submitButton'
+//           type='submit' 
+//           value='Add User' 
+//         />
+//       </form>
+//     )
+//   }
+// }
+
+// function InformationTable(props) {
+//   return (
+//     <table style={style.table} className='informationTable'>
+//       <thead> 
+//         <tr>
+//           <th style={style.tableCell}>First name</th>
+//           <th style={style.tableCell}>Last name</th>
+//           <th style={style.tableCell}>Phone</th>
+//         </tr>
+//       </thead> 
+
+
+
+//       <tbody>
+//         {props.phoneBook.map(entry => {
+//             console.table(entry)
+//           return (
+//             <tr>
+//               <td style={style.tableCell}>{entry.userFirstname}</td>
+//               <td style={style.tableCell}>{entry.userLastname}</td>
+//               <td style={style.tableCell}>{entry.userPhone}</td>
+//             </tr>
+//           )
+//         })}
+//       </tbody>
+//     </table>
+//   );
+
+// }
+
+// class Application extends Component {
+//   state = {
+//     phoneBook: []
+//   }
+
+//   addEntry = (data) => {
+//     const added = this.state.phoneBook.concat(data)
+//     this.setState({ 
+//       phoneBook: added
+//     })
+//   }
+
+//   render() {
+//     return (
+//       <section>
+//         <PhoneBookForm addEntry={this.addEntry}/>
+//         <InformationTable phoneBook={this.state.phoneBook}/>
+//       </section>
+//     );
+//   }
+// }
+
+// ReactDOM.render(
+//   <Application />,
+//   document.getElementById('root')
+// );
+
+
+
   
